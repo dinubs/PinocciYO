a35dff6d62149c92b57069ed3228664be6d1ed79
Gavin Dinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-08-14 15:04:31 -0700

    Working on the README

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var pinoccio = require('pinoccio');
 var http = require("http");
 http.post = require('http-post');
 
-var api = pinoccio('a59def26f756f89b8796813dd241d980');
+var api = pinoccio(// Pinoccio API Toke Here);
 
 var s = api.sync();
 
@@ -17,7 +17,7 @@ s.on('data', function(data){
   data = data.data;
   if (data.type === 'digital' && data.troop === '1' && data.scout === '1') {
     if (data.value.state[1] == 0) {
-      http.post('http://api.justyo.co/yoall', {api_token: "53ef2ceb-4972-7eb8-2032-344788e152d4"});  
+      http.post('http://api.justyo.co/yoall', {api_token: //YO API Token Here});  
       console.log("Someone rang the doorbell".help);
       command = "led.green delay(5000) led.off";
       api.rest({url:"/v1/1/1/command", data:{command:command}},function(err,data){});
