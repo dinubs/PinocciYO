f1acd1ee907364f6a5fdf32d1f6700351fcb0192
Gavin Dinubilo <gavin_dinubilo@mail.tmcc.edu>
2014-08-14 14:52:54 -0700

    updated javascript

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ s.on('data', function(data){
   if (data.type === 'digital' && data.troop === '1' && data.scout === '1') {
     if (data.value.state[1] == 0) {
       http.post('http://api.justyo.co/yoall', {api_token: "53ef2ceb-4972-7eb8-2032-344788e152d4"});  
-      console.log("Someone rang the door".help);
+      console.log("Someone rang the doorbell".help);
       command = "led.green delay(5000) led.off";
       api.rest({url:"/v1/1/1/command", data:{command:command}},function(err,data){});
     }
